53d06bc19b2ffc3bdfc83c3d97049d8b28f97b66
s92mike <migyn.s92mike@gmail.com>
2018-02-09 13:12:04 +0800

    finalized static page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends Component {
-  render(){
-    return (<div className="h2">
-      Hello
-    </div>)
-  }
+    render() {
+        return(<div className="card">
+            <h3 className="card-header">Header Hell0 WorLd!!!</h3>
+            <div className="card-body">
+                <h5 className="card-title">Title </h5>
+                <p className="card-text">Lorim IpsumLorim IpsumLorim IpsumLorim IpsumLorim IpsumLorim Ipsum</p>
+                <a href="#" className="btn btn-primary">Go to the world</a>
+            </div>  
+        </div>) 
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
